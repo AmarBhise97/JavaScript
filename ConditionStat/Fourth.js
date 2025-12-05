76ef72be6cc7c4a73f840ef4df3bafbd7e12e250
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-05 22:03:11 +0530

    If-else-lader

---
+++ ConditionStat/Fourth.js
@@ -0,0 +1,34 @@
+let marks=91;
+let isAddimisionconformed=false;
+
+if(isAddimisionconformed=true){
+    if(marks>50 && marks<=60){
+        console.log("Grade D..");
+        
+    }
+    else if(marks>60 && marks<=70){
+        console.log("Grade c..");
+        
+    }
+    else if(marks>70 && marks<=80){
+        console.log("Grade B.. ");
+        
+    }
+    else if(marks>80 && marks<=90){
+        console.log("Grade A..");
+        
+    }
+    else if(marks>90 && marks<=100){
+        console.log("Grade A++");
+        
+    }
+    else{
+        console.log("Failed or Invalid marks");
+        
+    }
+
+}
+else{
+    console.log("Please take the addimision first !");
+    
+}
