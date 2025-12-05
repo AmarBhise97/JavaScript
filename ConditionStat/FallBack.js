edc7ec31b0122986d6e0d2bedc4f8860a5c970f6
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-05 23:28:05 +0530

    Update

--- ConditionStat/FallBack.js
+++ ConditionStat/FallBack.js
@@ -7,6 +7,6 @@
 
 //false ,Undefined,null
 
-let islogged= null ;
+let islogged= null ;/// using false ,undefined,null than output will you are not logged 
 let Accepted = islogged || "you are not logged";
 console.log(Accepted);
---
+++ ConditionStat/Nullish.js
@@ -0,0 +1,11 @@
+/**
+ * Nullish coleascion ==>null undefined
+ * 
+ */
+
+let marks="Amar";//ussing  null and undefined than print not defined another use can print as-it-is.
+let result;
+
+result = marks??"Not Defined"
+
+console.log("Result  =  "+result);
