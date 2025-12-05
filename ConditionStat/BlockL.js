bc05f0c7982f52dd7b5ef095d412fa38f3612a52
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-05 22:13:40 +0530

    Blocklevel

---
+++ ConditionStat/BlockL.js
@@ -0,0 +1,22 @@
+/**
+ * Scope:
+ *        1.Global
+ *        2.Block level
+ *        3.Function
+ * 
+ * let ,const==> Bolck level,Global,Function level
+ * var ==> Global,Function level
+ */
+if(true){
+    /**
+     * Block level
+     */
+    let number =10;
+    const number1=49;
+    var number3=59;
+}
+//console.log(number);==>let......number is not deffined
+//console.log(number1);==> const... number1 is not deffined
+console.log(number3);
+
+
