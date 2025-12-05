aea35d9c6c3ed9cc5798d8f279c2194b93646da5
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-05 23:21:46 +0530

    Update

---
+++ ConditionStat/FallBack.js
@@ -0,0 +1,12 @@
+/**
+ * flase value fallback.
+ * Nullesh coealscion
+ * Default value(Ecama Script 2020)
+ */
+
+
+//false ,Undefined,null
+
+let islogged= null ;
+let Accepted = islogged || "you are not logged";
+console.log(Accepted);
