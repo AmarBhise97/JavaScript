4476b11d632473f7d4ebce7561cd98416ae80d86
Amar bhise <bhiseamarwagholi@gmail.com>
2025-12-25 18:30:35 +0530

    Prime_1

---
+++ Loop/Prime_1.js
@@ -0,0 +1,16 @@
+
+var flag =false;
+for(var v=2;v<13;v++){
+    if(13%v==0){
+       flag = true; 
+    }
+
+}
+if(flag==true){
+    console.log("Not Prime");
+    
+}
+else{
+    console.log("Prime");
+    
+}
