7ea63f0b947c13a65e01c748a5a303fe794178bd
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-19 19:43:44 +0530

    Object

---
+++ Object/First.js
@@ -0,0 +1,19 @@
+let pins = {
+    "Amar": 6042003,
+    "Bhise":7045676,
+    rahul:34567,
+    pratik:"ratima",
+    Demo : [1,2,5,6,8],
+
+   Greet: function(){
+                console.log("Hello,human");
+   }
+};
+
+console.log(pins.Amar);
+console.log(pins.Demo);
+console.log(pins['Amar']);
+console.log(pins.Greet);
+
+
+
