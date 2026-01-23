69d11dc3e0b06802581df0eb43750f9055cb94a1
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-23 23:34:53 +0530

    Delete

---
+++ Object/Fourthdelete.js
@@ -0,0 +1,12 @@
+let student= {
+
+    name:"Amar",
+    Mobile:9730567675,
+    email:"amar@gmail.com",
+    address:"kolkate"
+
+}
+console.log(delete student.address);//true and false will return.
+
+delete student.address
+console.log(student);
