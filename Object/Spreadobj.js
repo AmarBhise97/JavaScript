d390b2daa1699bb951db6876319c63339cfc3816
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-23 23:45:09 +0530

    Spreadobj

---
+++ Object/Spreadobj.js
@@ -0,0 +1,14 @@
+let obj1={
+    name:"Amar",
+    Mobile:9730567675,
+    email:"amar@gmail.com",
+    address:"kolkate"
+}
+
+
+let obj2={
+    incode :121234546
+
+}
+let obj3={...obj1,...obj2}
+console.log(obj3);
