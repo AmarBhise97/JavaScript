0c3374bea651dc69042b5702f1033270767df04d
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-26 23:08:13 +0530

    Revision

---
+++ Object/Revision.js
@@ -0,0 +1,19 @@
+let obj={
+    1:"Amar",
+    2:"Bhise",
+    3:"Rahul",
+
+    4:"Pratik",
+   // 5:arr=[1,2,3,4,5]   
+
+
+};
+console.log(obj[4].length);
+console.log(obj[1].length);
+
+
+let arr=[10,20,30,40,50,60];
+let [a,b,c,d,e,f]=arr;
+console.log(a);
+
+
