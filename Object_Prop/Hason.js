d394ef4221ff0299cfd993e155977ae0cf6dc174
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-28 23:31:07 +0530

    Object Properties

---
+++ Object_Prop/Hason.js
@@ -0,0 +1,15 @@
+let Obj={
+
+    Name:"John",
+    age:30,
+
+    city:"Chicago",
+
+
+}
+
+console.log();
+
+console.log(Object.hasOwn(Obj,"Name"));//true
+console.log(Object.hasOwn(Obj,"country"));//false
+
