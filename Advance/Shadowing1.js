1501c7142af362192fb7cc17ee2f9d7161ffd1de
Amar bhise <bhiseamarwagholi@gmail.com>
2026-01-30 00:24:26 +0530

    Shadowing

---
+++ Advance/Shadowing1.js
@@ -0,0 +1,33 @@
+let name =" Amar";
+{
+    let name="rutik";
+    console.log(name);
+    
+}
+console.log(name);
+
+console.log("======================================================================");
+
+var name2="gagan";
+{
+    let name2="vivek";
+    console.log(name2);
+}
+console.log(name2);
+
+console.log("/////////////////////////////////////////////////////////////////////");
+var name3="gagan";
+{
+    var name3="vivek";
+    console.log(name3);
+}
+console.log(name3);
+
+console.log("____________________________________________________________________");
+const name4="Pratik";
+{
+    const name4="sanket";
+    console.log(name4);
+    
+}
+console.log(name4);
